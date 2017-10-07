e495e05c10fd9f92d044e467639f29c1b3c73924
lorstevens <lornacstevens@gmail.com>
2017-10-07 12:12:50 -0500

    changes

---
+++ keys.js
@@ -0,0 +1,11 @@
+// console.log('this is loaded');
+
+var twitterKeys =  {
+  consumer_key: 'lVmbRFbeywRMRAEv5d6X1tBdZ',
+  consumer_secret: 'sX0kCXKHUoR6GfbklFlwaBw5obQaAySCTYG9H2oZOcrIhDcEMI',
+  access_token_key: '915588149442727936-xZh9dv6iIVxPYMEmZ6fY7vMsSa0YQBG',
+  access_token_secret: 'fUo1khKhd09mvF9MODz7Rrg71j5gkfUuB8r3OUMBiboUf',
+}
+
+
+module.exports = twitterKeys;
