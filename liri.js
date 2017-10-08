234425c013a755e755fa63ddedf9a84ee235fa9a
lorstevens <lornacstevens@gmail.com>
2017-10-08 14:42:50 -0500

    changes to readfile

--- liri.js
+++ liri.js
@@ -4,20 +4,40 @@ var request = require("request");
 var twitter = require ("twitter");
 var spotify = require ("node-spotify-api");
 var keys = require("./keys.js");
-var nodeArg = process.argv[2];
+var firstArg = process.argv[2];
 
 
 
-if(nodeArg === "my-tweets"){
+
+if(firstArg === "my-tweets"){
   runTwitter();
 }
-else if(nodeArg === "spotify-this-song"){
+else if(firstArg === "spotify-this-song"){
+  if (process.argv[3]){
+  var value = "";
+  for (var n = 3; n < process.argv.length; n++){
+      value += " " + process.argv[n];
+    }
+    };
   runSpotify();
 }
-else if(nodeArg === "movie-this"){
+else if(firstArg === "movie-this"){
+  if (process.argv[3]){
+  var value = "";
+  for (var n = 3; n < process.argv.length; n++){
+      value += " " + process.argv[n];
+    }
+    };
   runOMDB();
 }
-else if(nodeArg === "do-what-it-says"){
+
+else if(firstArg === "do-what-it-says"){
+    if (process.argv[3]){
+  var value = "";
+  for (var n = 3; n < process.argv.length; n++){
+      value += " " + process.argv[n];
+    }
+    };
   runDoIt();
 }
 else {
@@ -55,12 +75,12 @@ function runTwitter(){
 
 
 function runSpotify (){
-var secondArg = process.argv[3];
+
  
 var spotifyAPI = new spotify(keys.spotifyKeys);
  
 
- spotifyAPI.search({ type: 'track', query: secondArg || "The Sign Ace of Base"}, function(err, data) {
+ spotifyAPI.search({ type: 'track', query: value || "The Sign Ace of Base"}, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -79,12 +99,17 @@ var spotifyAPI = new spotify(keys.spotifyKeys);
 
 
 function runOMDB (){
-  var secondArg = process.argv[3];
+  if (value === undefined) {
+   value = 'Mr Nobody';
+  }
+ 
 // Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=" + secondArg + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
   // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode === 200) {
+
+
 
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
@@ -98,5 +123,36 @@ request("http://www.omdbapi.com/?t=" + secondArg + "&y=&plot=short&apikey=40e9ce
     console.log("Actors: " + JSON.parse(body).Actors);
 
   }
+
+
+
 });
 }
+
+
+function runDoIt (){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+
+  else {
+  
+    var spotifyAPI = new spotify(keys.spotifyKeys);
+    var dataArr = data.split(",");
+
+    spotifyAPI.search({ type: 'track', query: dataArr[1]}, function(err, data) {
+  
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Preview Here: " + data.tracks.items[0].preview_url)
+ });
+}
+
+
+
+});
+}
+
